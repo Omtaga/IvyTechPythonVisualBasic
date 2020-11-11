538d6c215b1e3be6a4a077149dfab9700f09c702
Omtaga <andrew@andrewmpierce.com>
2020-11-10 20:49:09 -0500

    sketched in assignment.

---
+++ Chapter 005 Exercises/PierceAndrewM03_Ch5Ex12.py
@@ -0,0 +1,53 @@
+# Ivy Tech - SDEV 140 - Introduction to Software Development
+# Chapter 5 Exercise 12. Maximum of Two Values
+# Andrew M. Pierce  Associate of Applied Science - Software Development
+# Python 3.8.6
+
+# logging for exceptions / sys for quit / time for sleep
+import logging
+import sys
+import time
+
+# Handles all user input, expects numeric entry and responses > 0
+
+
+def inputHandling(question):
+    while True:
+        try:
+            userInput = input(question)
+            isinstance(float(userInput), str)
+        except Exception:
+            logging.exception('Caught an error')
+            # Pauses program so that exception prints to screen
+            # and user sees next step to continue after error message.
+            time.sleep(1)
+            print("Your number needs to be entered with numeric keys.")
+            userQuit = input("Hit enter to continue or type 'q' and enter to quit: ")
+            if userQuit == 'q':
+                sys.exit("Quitting Program")
+        else:
+            # Makes sure input is greater than zero to avoid / by zero error or zero responses
+            if float(userInput) <= 0:
+                print("Your input must be greater than zero.")
+                userQuit = input("Hit enter to continue or type 'q' and enter to quit: ")
+                if userQuit == 'q':
+                    sys.exit("Quitting Program")
+            else:
+                return float(userInput)
+
+
+def maximumOfTwoValues(valueOne, valueTwo):
+    # This function always returns a number, if both numbers are equal it will return the first number.
+    if valueOne > valueTwo:
+        return valueOne
+    elif valueTwo > valueOne:
+        return valueTwo
+    else:
+        return valueOne
+
+
+def main():
+    pass
+
+
+main()
