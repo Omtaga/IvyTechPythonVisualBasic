44ed2eaf10bd8f591e52195a550845872a946d08
Omtaga <andrew@andrewmpierce.com>
2020-11-19 13:57:18 -0500

    midterm cleanup totalSales finished

---
+++ Chapter 007 Exercises/PierceAndrewM04_Ch7Ex1.py
@@ -0,0 +1,69 @@
+# Ivy Tech - SDEV 140 - Introduction to Software Development
+# Chapter 7 Exercise 1. Total Sales
+# Andrew M. Pierce  Associate of Applied Science - Software Development
+# Python 3.8.6
+
+import sys
+import logging
+import time
+
+
+def inputHandling(question):
+    while True:
+        try:
+            typedAmount = input(question)
+            isinstance(float(typedAmount), str)
+        except ValueError:
+            logging.exception('Caught an error')
+            # Pauses program so that exception prints to screen
+            # and user sees next step to continue after error message.
+            time.sleep(1)
+            print("Your number needs to be entered with numeric keys.")
+            userQuit = input("Hit enter to continue or type 'q' and enter to quit: ")
+            if userQuit == 'q':
+                sys.exit("Quitting Program")
+        else:
+            return float(typedAmount)
+
+
+def userInput(week):
+    day = 0
+    inputAmount = []
+    continueInput = True
+    while continueInput and day < 7:
+        word = inputHandling(f"Please enter the sales for {week[day]}: ")
+        inputAmount.append(word)
+        if day < 6:
+            yesNo = input('Type "y" to continue or anything else to stop: ')
+            if yesNo == "Yes" or yesNo == "yes" or yesNo == "Y" or yesNo == "y":
+                continueInput = True
+            else:
+                continueInput = False
+        day += 1
+    return inputAmount, day - 1
+
+
+def computeSalesTotal(weeklyTotal):
+    result = 0
+    for dailyTotal in weeklyTotal:
+        result += float(dailyTotal)
+    return result
+
+
+def main():
+    week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    salesTotalAndDay = userInput(week)
+    salesTotal = computeSalesTotal(salesTotalAndDay[0])
+    day = salesTotalAndDay[1]
+    if salesTotal == 0:
+        print('There were no sales for this week.')
+    elif day == 0:
+        print(f'Sales for Sunday were ${salesTotal:,.2f}.')
+    elif day == 1:
+        print(f'Sales for Sunday and Monday were ${salesTotal:,.2f}.')
+    else:
+        print(f'Sales for Sunday thru {week[day]} was ${salesTotal:,.2f}.')
+
+
+if __name__ == '__main__':
+    main()
--- Chapter 008 Midterm/PierceAndrewM04_Midterm.py
+++ Chapter 008 Midterm/PierceAndrewM04_Midterm.py
@@ -4,35 +4,54 @@
 # Python 3.8.6
 
 
-def main():
-    result = ""
-    inputList = []
+def userInput():
+    wordsInput = []
     continueInput = True
     while continueInput:
         word = input("Please type a word you would like to add to your sentence: ")
         if word == "i":
             word = "I"
-        inputList.append(word)
+        wordsInput.append(word)
         yesNo = input('Type "y" to continue or anything else to stop: ')
         if yesNo == "Yes" or yesNo == "yes" or yesNo == "Y" or yesNo == "y":
             continueInput = True
         else:
             continueInput = False
-    inputList[0] = inputList[0].capitalize()
-    for index in range(len(inputList)):
-        if index == len(inputList) - 1:
-            result += inputList[index]
+    return wordsInput
+
+
+def combineWords(inputWords):
+    finishedSentence = ""
+    inputWords[0] = inputWords[0].capitalize()
+    for wordIndex in range(len(inputWords)):
+        if wordIndex == len(inputWords) - 1:
+            finishedSentence += inputWords[wordIndex]
         else:
-            result += inputList[index] + " "
-    print(f'{result}.')
-    plural = ""
-    isAre = ""
-    if len(inputList) == 1:
-        isAre += "is"
+            finishedSentence += inputWords[wordIndex] + " "
+    return finishedSentence
+
+
+def defineIsAre(inputWords):
+    if len(inputWords) == 1:
+        return "is"
     else:
-        isAre += "are"
-        plural += "s"
-    print(f'There {isAre} {len(inputList)} word{plural} in the sentence.')
+        return "are"
+
+
+def definePlural(inputWords):
+    if len(inputWords) == 1:
+        return ""
+    else:
+        return "s"
+
+
+def main():
+    inputWords = userInput()
+    sentence = combineWords(inputWords)
+    plural = definePlural(inputWords)
+    isAre = defineIsAre(inputWords)
+    print(f'{sentence}.')
+    print(f'There {isAre} {len(inputWords)} word{plural} in the sentence.')
 
 
 if __name__ == "__main__":
