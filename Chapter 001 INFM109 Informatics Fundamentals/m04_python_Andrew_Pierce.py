7ccf89500f79c77013f8489ec151d0a5c202317b
Omtaga <andrew@andrewmpierce.com>
2020-11-24 15:27:23 -0500

    primeNumber file for INFM109

--- Chapter 001 INFM109 Informatics Fundamentals/m04_python_Andrew_Pierce.py
+++ Chapter 001 INFM109 Informatics Fundamentals/m04_python_Andrew_Pierce.py
@@ -3,6 +3,10 @@
 # Andrew M. Pierce  Associate of Applied Science - Software Development
 # Python 3.9.0
 
+# Time for sleep so user gets next response if error message in console.
+# Sys for exit should user opt to exit program.
+# Logging for handling errors.
+# Datetime used for grammar.
 import time
 import sys
 import logging
@@ -35,9 +39,9 @@ def inputHandling(question):
 
 
 def main():
-    print()
-    userYear = inputHandling('Enter a year to determine if it is a leap year:  ')
+    userYear = inputHandling('\nEnter a year to determine if it is a leap year:  ')
 
+    # Initialize variables early for some results.
     bc = ''
     nullYear = None
     noLeap = None
@@ -45,8 +49,10 @@ def main():
 
     # This section handles the special cases that arose during the implementation of leap year.
     if userYear == 0:
+        # Creates output for user input of zero.
         nullYear = "There was no year 0!"
     elif userYear < -45:
+        # Creates output for user input less than -45.
         noLeap = f'In {userYear * -1} BC it was not a leap year.  Leap year was not introduced until 45 BC'
     elif userYear == 8 or userYear == 12:
         numberOf = 29
@@ -62,8 +68,8 @@ def main():
         numberOf = 28
 
     # This section handles the most recent years and those going forward until adjustments are needed.
-    if userYear >= 16 and bc != ' BC':
-        if userYear % 100 == 0 and userYear % 400 == 0:
+    if userYear >= 16:
+        if userYear % 400 == 0:
             numberOf = 29
         elif userYear % 4 == 0 and userYear % 100 == 0:
             numberOf = 28
@@ -72,7 +78,7 @@ def main():
         else:
             numberOf = 28
 
-    # Handles grammar in last output.
+    # Handles Have, Has, and had in last output.
     now = datetime.datetime.now()
     if userYear > now.year:
         haveHadHas = 'will have'
@@ -81,6 +87,7 @@ def main():
     else:
         haveHadHas = 'has'
 
+    # Handles the word 'be' in the final output.
     if userYear > now.year:
         be = ' be '
     elif userYear < now.year:
@@ -88,6 +95,7 @@ def main():
     else:
         be = ' '
 
+    # Handles will, was, and is in the final output.
     if userYear > now.year:
         wasIsWill = 'will'
     elif userYear < now.year:
@@ -95,17 +103,24 @@ def main():
     else:
         wasIsWill = 'is'
 
+    # Output begins here.
     if nullYear is not None:
+        # Pulls response from user input of zero.
         output = nullYear
+        print(output)
     elif noLeap is not None:
+        # Pull response from user input of less than -45 (45BC)
         output = noLeap
+        print(output)
     else:
         output = f'In {userYear}{bc}, February {haveHadHas} {numberOf} days.'
+        print(output)
+        if numberOf == 28:
+            print(f'{userYear} {wasIsWill} not{be}a leap year.')
+        else:
+            print(f'{userYear} {wasIsWill}{be}a leap year.')
+    input('\nPress enter to exit...')
 
-    print(output)
-    print()
-    input('Press enter to exit...')
-    print()
 
-
-main()
+if __name__ == "__main__":
+    main()
